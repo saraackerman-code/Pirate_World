800ec30e35c9d9c46379b40ed3ed46c3ec1dfd2e
Sara Ackerman <saralux004@GMAIL.HOTMAIL>
2026-01-07 15:17:44 +0000

    Edits made

--- SourceCode/settings.py
+++ SourceCode/settings.py
@@ -2,17 +2,17 @@ import pygame, sys
 from pygame.math import Vector2 as vector
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-TITLE_SIZE = 64
+TILE_SIZE = 64
 ANIMATION_SPEED = 6
 
-#Layers
-Z_LAYERS ={
-    'bg':0,
-    'clouds':1,
-    'bg tiles':2,
-    'path':3,
-    'bg details':4,
-    'main': 5,
-    'water': 6,
-    'fg':7
+# layers
+Z_LAYERS = {
+	'bg': 0,
+	'clouds': 1,
+	'bg tiles': 2,
+	'path': 3,
+	'bg details': 4,
+	'main': 5,
+	'water': 6,
+	'fg': 7
 }
